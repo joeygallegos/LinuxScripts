8d69fe727b8a6a8a1a01ac3c62fa22927be6710b
Joey Gallegos <joey@joeygallegos.com>
2024-06-18 09:34:25 -0500

    Add mailgun and configs

--- monitor_apache.sh
+++ monitor_apache.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
 
-# /var/run/apache2/apache2.pid
-env_file='/home/monitor_apache_config.env'
+# Paths to configuration files
+apache_config_file='/home/monitor_apache_config.json'
+mailgun_config_file='/home/config.json'
 
+# Function to check and create the configuration file if it doesn't exist
 do_credentials_config_check() {
-  if [[ ! -f "$env_file" ]]; then
-    echo 'Config file does not exist - creating one'
-    touch "$env_file"
+  if [[ ! -f "$apache_config_file" ]]; then
+    echo 'Apache config file does not exist - creating one'
+    touch "$apache_config_file"
+  fi
+  if [[ ! -f "$mailgun_config_file" ]]; then
+    echo 'Mailgun config file does not exist - creating one'
+    touch "$mailgun_config_file"
   fi
 }
 
-# check the apache pid three times per second
-# if the pid is not found, then try to start apache three times
+# Function to send email using Mailgun
+send_mailgun_email() {
+  local subject="$1"
+  local body="$2"
+  local to_email="$3"
+
+  local mailgun_api_key=$(jq -r '.mailgun_api_key' "$mailgun_config_file")
+  local mailgun_domain=$(jq -r '.mailgun_domain' "$mailgun_config_file")
+  local mailgun_from=$(jq -r '.mailgun_from' "$mailgun_config_file")
+
+  curl -s --user "api:$mailgun_api_key" \
+    https://api.mailgun.net/v3/"$mailgun_domain"/messages \
+    -F from="$mailgun_from" \
+    -F to="$to_email" \
+    -F subject="$subject" \
+    -F text="$body"
+}
+
+# Function to check the Apache PID and try to restart Apache if necessary
 do_apache_check() {
-    process_id=`cat "$env_file" | cut -d '|' -f1`
-    server_name=`cat "$env_file" | cut -d '|' -f2`
-    server_ip=`cat "$env_file" | cut -d '|' -f3`
-    notification_email=`cat "$env_file" | cut -d '|' -f4`
+    local process_id=$(jq -r '.process_id_file' "$apache_config_file")
+    local server_name=$(hostname)
+    local server_ip=$(jq -r '.server_ip' "$apache_config_file")
+    local notification_email=$(jq -r '.notification_email' "$apache_config_file")
 
     if ! [ -f "$process_id" ]; then
         systemctl start apache2.service
@@ -34,7 +57,7 @@ do_apache_check() {
 
     sleep 10s
     if ! [ -f "$process_id" ]; then
-        mail -s 'Apache is down' $notification_email <<< "Apache is down on $server_name ($server_ip) and cannot be restarted"
+        send_mailgun_email 'Apache is down' "Apache is down on $server_name ($server_ip) and cannot be restarted" "$notification_email"
     fi
 }
 
